09316c1a48e053ac0948eeb1dd977543ebf35c8c
Kiëd Llaentenn <kiedtl@tilde.team>
2021-02-08 23:17:22 +0000

    main.c: Fix atrocious formatting

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
  * Copyright (C) 1980 by Michael Toy and Glenn Wichman
  * All rights reserved
  *
- * @(#)main.c	3.27 (Berkeley) 6/15/81
+ * @(#)main.c		3.27 (Berkeley) 6/15/81
  */
 
 #include "curses.h"
@@ -17,322 +17,322 @@
 #include "rogue.h"
 
 struct termios terminal;
-WINDOW *cw;                              /* Window that the player sees */
-WINDOW *hw;                              /* Used for the help command */
-WINDOW *mw;                              /* Used to store mosnters */
+WINDOW *cw;               /* Window that the player sees */
+WINDOW *hw;               /* Used for the help command */
+WINDOW *mw;               /* Used to store mosnters */
 
-main(argc, argv, envp)
-char **argv;
-char **envp;
+int
+main(argc, char **argv, char **envp)
 {
-    register char *env;
-    register struct passwd *pw;
-    register struct linked_list *item;
-    register struct object *obj;
-    struct passwd *getpwuid();
-    char *getpass();
-    int lowtime;
-    time_t now;
+	char *env;
+	struct passwd *pw;
+	struct linked_list *item;
+	struct object *obj;
+	int lowtime;
+	time_t now;
 
 #ifdef __DJGPP__
-    _fmode = O_BINARY;
+	_fmode = O_BINARY;
 #endif
 
-    /*
-     * check for print-score option
-     */
-    if (argc == 2 && strcmp(argv[1], "-s") == 0)
-    {
-	waswizard = TRUE;
-	score(0, -1);
-	exit(0);
-    }
-
-    /*
-     * get home and options from environment
-     */
-    if ((env = getenv("HOME")) != NULL)
-	strcpy(home, env);
-    else if ((pw = getpwuid(getuid())) != NULL)
-	strcpy(home, pw->pw_dir);
-    else
-	home[0] = '\0';
-    strcat(home, "/");
-
-    strcpy(file_name, home);
-    strcat(file_name, "rogue.sav");
-
-    if ((env = getenv("ROGUEOPTS")) != NULL)
-	parse_opts(env);
-    if (env == NULL || whoami[0] == '\0')
-	if ((pw = getpwuid(getuid())) == NULL)
-	{
-	    printf("Say, who the hell are you?\n");
-	    exit(1);
+	/*
+	 * check for print-score option
+	 */
+	if (argc == 2 && strcmp(argv[1], "-s") == 0) {
+		waswizard = TRUE;
+		score(0, -1);
+		exit(0);
 	}
+
+	/*
+	 * get home and options from environment
+	 */
+	if ((env = getenv("HOME")) != NULL)
+		strcpy(home, env);
+	else if ((pw = getpwuid(getuid())) != NULL)
+		strcpy(home, pw->pw_dir);
 	else
-	    strucpy(whoami, pw->pw_name, strlen(pw->pw_name));
-    if (env == NULL || fruit[0] == '\0')
-	strcpy(fruit, "slime-mold");
+		home[0] = '\0';
+	strcat(home, "/");
 
-    if (argc == 2)
-	if (!restore(argv[1], envp)) /* Note: restore will never return */
-	{
-	    endwin();
-	    exit(1);
+	strcpy(file_name, home);
+	strcat(file_name, "rogue.sav");
+
+	if ((env = getenv("ROGUEOPTS")) != NULL)
+		parse_opts(env);
+
+	if (env == NULL || whoami[0] == '\0') {
+		if ((pw = getpwuid(getuid())) == NULL) {
+			printf("Say, who the hell are you?\n");
+			exit(1);
+		} else {
+			strucpy(whoami, pw->pw_name, strlen(pw->pw_name));
+		}
 	}
-    time(&now);
-    lowtime = (int) now;
-    dnum = (wizard && getenv("SEED") != NULL ?
-	atoi(getenv("SEED")) :
-	lowtime + getpid());
-    if (wizard)
-	printf("Hello %s, welcome to dungeon #%d", whoami, dnum);
-    else
-	printf("Hello %s, just a moment while I dig the dungeon...", whoami);
-    fflush(stdout);
-    seed = dnum;
-    init_player();			/* Roll up the rogue */
-    init_things();			/* Set up probabilities of things */
-    init_names();			/* Set up names of scrolls */
-    init_colors();			/* Set up colors of potions */
-    init_stones();			/* Set up stone settings of rings */
-    init_materials();			/* Set up materials of wands */
-    initscr();				/* Start up cursor package */
 
-    if (COLS < 70)
-    {
-	printf("\n\nSorry, %s, but your terminal window has too few columns.\n", whoami);
-	printf("Your terminal has %d columns, needs 70.\n",COLS);
-	endwin();
-	exit(1);
-    }
-    if (LINES < 22)
-    {
-	printf("\n\nSorry, %s, but your terminal window has too few lines.\n", whoami);
-	printf("Your terminal has %d lines, needs 22.\n",LINES);
-	endwin();
-	exit(1);
-    }
-    
+	if (env == NULL || fruit[0] == '\0')
+		strcpy(fruit, "slime-mold");
 
-    setup();
-    /*
-     * Set up windows
-     */
-    cw = newwin(LINES, COLS, 0, 0);
-    mw = newwin(LINES, COLS, 0, 0);
-    hw = newwin(LINES, COLS, 0, 0);
-    waswizard = wizard;
-    new_level();			/* Draw current level */
-    /*
-     * Start up daemons and fuses
-     */
-    daemon(doctor, 0, AFTER);
-    fuse(swander, 0, WANDERTIME, AFTER);
-    daemon(stomach, 0, AFTER);
-    daemon(runners, 0, AFTER);
-    /*
-     * Give the rogue his weaponry.  First a mace.
-     */
-    item = new_item(sizeof *obj);
-    obj = (struct object *) ldata(item);
-    obj->o_type = WEAPON;
-    obj->o_which = MACE;
-    init_weapon(obj, MACE);
-    obj->o_hplus = 1;
-    obj->o_dplus = 1;
-    obj->o_flags |= ISKNOW;
-    add_pack(item, TRUE);
-    cur_weapon = obj;
-    /*
-     * Now a +1 bow
-     */
-    item = new_item(sizeof *obj);
-    obj = (struct object *) ldata(item);
-    obj->o_type = WEAPON;
-    obj->o_which = BOW;
-    init_weapon(obj, BOW);
-    obj->o_hplus = 1;
-    obj->o_dplus = 0;
-    obj->o_flags |= ISKNOW;
-    add_pack(item, TRUE);
-    /*
-     * Now some arrows
-     */
-    item = new_item(sizeof *obj);
-    obj = (struct object *) ldata(item);
-    obj->o_type = WEAPON;
-    obj->o_which = ARROW;
-    init_weapon(obj, ARROW);
-    obj->o_count = 25+rnd(15);
-    obj->o_hplus = obj->o_dplus = 0;
-    obj->o_flags |= ISKNOW;
-    add_pack(item, TRUE);
-    /*
-     * And his suit of armor
-     */
-    item = new_item(sizeof *obj);
-    obj = (struct object *) ldata(item);
-    obj->o_type = ARMOR;
-    obj->o_which = RING_MAIL;
-    obj->o_ac = a_class[RING_MAIL] - 1;
-    obj->o_flags |= ISKNOW;
-    cur_armor = obj;
-    add_pack(item, TRUE);
-    /*
-     * Give him some food too
-     */
-    item = new_item(sizeof *obj);
-    obj = (struct object *) ldata(item);
-    obj->o_type = FOOD;
-    obj->o_count = 1;
-    obj->o_which = 0;
-    add_pack(item, TRUE);
-    playit();
+	if (argc == 2) {
+		if (!restore(argv[1], envp)) { /* Note: restore will never return */
+			endwin();
+			exit(1);
+		}
+	} 
+
+	time(&now);
+	lowtime = (int) now;
+	dnum = (wizard && getenv("SEED") != NULL ?
+		atoi(getenv("SEED")) :
+		lowtime + getpid());
+
+	if (wizard)
+		printf("Hello %s, welcome to dungeon #%d", whoami, dnum);
+	else
+		printf("Hello %s, just a moment while I dig the dungeon...", whoami);
+
+	fflush(stdout);
+	seed = dnum;
+	init_player();        /* Roll up the rogue */
+	init_things();        /* Set up probabilities of things */
+	init_names();         /* Set up names of scrolls */
+	init_colors();        /* Set up colors of potions */
+	init_stones();        /* Set up stone settings of rings */
+	init_materials();     /* Set up materials of wands */
+	initscr();            /* Start up cursor package */
+
+	if (COLS < 70) {
+		printf("\n\nSorry, %s, but your terminal window has too few columns.\n", whoami);
+		printf("Your terminal has %d columns, needs 70.\n",COLS);
+		endwin();
+		exit(1);
+	}
+
+	if (LINES < 22) {
+		printf("\n\nSorry, %s, but your terminal window has too few lines.\n", whoami);
+		printf("Your terminal has %d lines, needs 22.\n",LINES);
+		endwin();
+		exit(1);
+	}
+
+	setup();
+
+	/*
+	 * Set up windows
+	 */
+	cw = newwin(LINES, COLS, 0, 0);
+	mw = newwin(LINES, COLS, 0, 0);
+	hw = newwin(LINES, COLS, 0, 0);
+	waswizard = wizard;
+	new_level();						/* Draw current level */
+
+	/*
+	 * Start up daemons and fuses
+	 */
+	daemon(doctor, 0, AFTER);
+	fuse(swander, 0, WANDERTIME, AFTER);
+	daemon(stomach, 0, AFTER);
+	daemon(runners, 0, AFTER);
+
+	/*
+	 * Give the rogue his weaponry.  First a mace.
+	 */
+	item = new_item(sizeof *obj);
+	obj = (struct object *) ldata(item);
+	obj->o_type = WEAPON;
+	obj->o_which = MACE;
+	init_weapon(obj, MACE);
+	obj->o_hplus = 1;
+	obj->o_dplus = 1;
+	obj->o_flags |= ISKNOW;
+	add_pack(item, TRUE);
+	cur_weapon = obj;
+
+	/*
+	 * Now a +1 bow
+	 */
+	item = new_item(sizeof *obj);
+	obj = (struct object *) ldata(item);
+	obj->o_type = WEAPON;
+	obj->o_which = BOW;
+	init_weapon(obj, BOW);
+	obj->o_hplus = 1;
+	obj->o_dplus = 0;
+	obj->o_flags |= ISKNOW;
+	add_pack(item, TRUE);
+
+	/*
+	 * Now some arrows
+	 */
+	item = new_item(sizeof *obj);
+	obj = (struct object *) ldata(item);
+	obj->o_type = WEAPON;
+	obj->o_which = ARROW;
+	init_weapon(obj, ARROW);
+	obj->o_count = 25+rnd(15);
+	obj->o_hplus = obj->o_dplus = 0;
+	obj->o_flags |= ISKNOW;
+	add_pack(item, TRUE);
+
+	/*
+	 * And his suit of armor
+	 */
+	item = new_item(sizeof *obj);
+	obj = (struct object *) ldata(item);
+	obj->o_type = ARMOR;
+	obj->o_which = RING_MAIL;
+	obj->o_ac = a_class[RING_MAIL] - 1;
+	obj->o_flags |= ISKNOW;
+	cur_armor = obj;
+	add_pack(item, TRUE);
+
+	/*
+	 * Give him some food too
+	 */
+	item = new_item(sizeof *obj);
+	obj = (struct object *) ldata(item);
+	obj->o_type = FOOD;
+	obj->o_count = 1;
+	obj->o_which = 0;
+	add_pack(item, TRUE);
+	playit();
 }
 
 /*
  * endit:
- *	Exit the program abnormally.
+ *		Exit the program abnormally.
  */
-
-void
+_Noreturn void
 endit(int p)
 {
-    fatal("Ok, if you want to exit that badly, I'll have to allow it\n");
+	fatal("Ok, if you want to exit that badly, I'll have to allow it\n");
 }
 
 /*
  * fatal:
- *	Exit the program, printing a message.
+ *		Exit the program, printing a message.
  */
-
-fatal(s)
-char *s;
+_Noreturn void
+fatal(char *s)
 {
-    clear();
-    move(LINES-2, 0);
-    printw("%s", s);
-    draw(stdscr);
-    endwin();
-    exit(0);
+	clear();
+	move(LINES-2, 0);
+	printw("%s", s);
+	draw(stdscr);
+	endwin();
+	exit(0);
 }
 
 /*
  * rnd:
- *	Pick a very random number.
+ *		Pick a very random number.
  */
-
-rnd(range)
-register int range;
+int
+rnd(int range)
 {
-    return range == 0 ? 0 : abs(RN) % range;
+	return range == 0 ? 0 : abs(RN) % range;
 }
 
 /*
  * roll:
- *	roll a number of dice
+ *		roll a number of dice
  */
-
-roll(number, sides)
-register int number, sides;
+int
+roll(int number, int sides)
 {
-    register int dtotal = 0;
-
-    while(number--)
-	dtotal += rnd(sides)+1;
-    return dtotal;
+	int dtotal = 0;
+	while(number--)
+		dtotal += rnd(sides)+1;
+	return dtotal;
 }
-# ifdef SIGTSTP
+
 /*
  * handle stop and start signals
  */
-
+# ifdef SIGTSTP
 void
 tstp(int p)
 {
-    mvcur(0, COLS - 1, LINES - 1, 0);
-    endwin();
-    fflush(stdout);
-    kill(0, SIGTSTP);
-    signal(SIGTSTP, tstp);
-    crmode();
-    noecho();
-    clearok(curscr, TRUE);
-    touchwin(cw);
-    draw(cw);
-    flush_type();	/* flush input */
+	mvcur(0, COLS - 1, LINES - 1, 0);
+	endwin();
+	fflush(stdout);
+	kill(0, SIGTSTP);
+	signal(SIGTSTP, tstp);
+	crmode();
+	noecho();
+	clearok(curscr, TRUE);
+	touchwin(cw);
+	draw(cw);
+	flush_type();		/* flush input */
 }
 # endif
 
-setup()
+void
+setup(void)
 {
 #ifndef DUMP
-    signal(SIGHUP, auto_save);
-    signal(SIGILL, auto_save);
-    signal(SIGTRAP, auto_save);
+	signal(SIGHUP, auto_save);
+	signal(SIGILL, auto_save);
+	signal(SIGTRAP, auto_save);
 #ifdef SIGIOT
-    signal(SIGIOT, auto_save);
+	signal(SIGIOT, auto_save);
 #endif
 #ifdef SIGEMT
-    signal(SIGEMT, auto_save);
+	signal(SIGEMT, auto_save);
 #endif
-    signal(SIGFPE, auto_save);
+	signal(SIGFPE, auto_save);
 #ifdef SIGBUS
-    signal(SIGBUS, auto_save);
+	signal(SIGBUS, auto_save);
 #endif
-    signal(SIGSEGV, auto_save);
+	signal(SIGSEGV, auto_save);
 #ifdef SIGSYS
-    signal(SIGSYS, auto_save);
+	signal(SIGSYS, auto_save);
 #endif
-    signal(SIGPIPE, auto_save);
-    signal(SIGTERM, auto_save);
+	signal(SIGPIPE, auto_save);
+	signal(SIGTERM, auto_save);
 #endif
 
-    signal(SIGINT, quit);
+	signal(SIGINT, quit);
 #ifndef DUMP
-    signal(SIGQUIT, endit);
+	signal(SIGQUIT, endit);
 #endif
 #ifdef SIGTSTP
-    signal(SIGTSTP, tstp);
+	signal(SIGTSTP, tstp);
 #endif
-    crmode();				/* Cbreak mode */
-    noecho();				/* Echo off */
+	crmode();		/* Cbreak mode */
+	noecho();		/* Echo off */
 }
 
 /*
  * playit:
- *	The main loop of the program.  Loop until the game is over,
+ *		The main loop of the program.  Loop until the game is over,
  * refreshing things and looking at the proper times.
  */
-
-playit()
+void
+playit(void)
 {
-    register char *opts;
+	register char *opts;
 
-    /*
-     * set up defaults for slow terminals
-     */
+	/*
+	 * set up defaults for slow terminals
+	 */
+	tcgetattr(0,&terminal);
 
-    tcgetattr(0,&terminal);
+	if (cfgetospeed(&terminal) < B1200) {
+		terse = TRUE;
+		jump = TRUE;
+	}
 
-    if (cfgetospeed(&terminal) < B1200)
-    {
-	terse = TRUE;
-	jump = TRUE;
-    }
-
-    /*
-     * parse environment declaration of options
-     */
-    if ((opts = getenv("ROGUEOPTS")) != NULL)
-	parse_opts(opts);
+	/*
+	 * parse environment declaration of options
+	 */
+	if ((opts = getenv("ROGUEOPTS")) != NULL)
+		parse_opts(opts);
 
 
-    oldpos = hero;
-    oldrp = roomin(&hero);
-    while (playing)
-	command();			/* Command execution */
-    endit(-1);
+	oldpos = hero;
+	oldrp = roomin(&hero);
+	while (playing)
+		command();         /* Command execution */
+	endit(-1);
 }
